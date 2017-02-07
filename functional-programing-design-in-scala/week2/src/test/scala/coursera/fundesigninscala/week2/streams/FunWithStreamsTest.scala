acdc8cde591b5dea8769625db7a28038cc942da7
Albert Ortiz <aortiz@dexmatech.com>
2017-02-07 16:34:46 +0100

    asd

---
+++ functional-programing-design-in-scala/week2/src/test/scala/coursera/fundesigninscala/week2/streams/FunWithStreamsTest.scala
@@ -0,0 +1,82 @@
+package coursera.fundesigninscala.week2.streams
+
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.collection.immutable.Seq
+
+/**
+  * Created by aortiz on 7/02/17.
+  */
+
+
+class FunWithStreamsTest extends FunSuite with Matchers {
+
+  // TODO: this class could be totally improved, but for now works in some cases at test
+  class SeqAccessRegistry {
+    var streamAccessCounter = 0
+    var listAccessCounter = 0
+
+    def streamRange(lo: Int, hi: Int): Stream[Int] = {
+      def _streamRange(lo: Int, hi: Int): Stream[Int] =
+        if (lo >= hi) Stream.empty
+        else {
+          streamAccessCounter = streamAccessCounter + 1
+          lo #:: _streamRange(lo + 1, hi)
+        }
+
+      streamAccessCounter = 0
+      _streamRange(lo, hi)
+    }
+
+    def listRange(lo: Int, hi: Int): List[Int] = {
+
+      def _listRange(lo: Int, hi: Int): List[Int] =
+        if (lo >= hi) Nil
+        else {
+          listAccessCounter = listAccessCounter + 1
+          lo :: _listRange(lo + 1, hi)
+        }
+
+      listAccessCounter = 0
+      _listRange(lo, hi)
+    }
+
+  }
+
+  test("stream creation recursive method should be called only one time if resulting stream is not accessed") {
+
+    // given
+    val registry = new SeqAccessRegistry
+
+    // when
+    val stream: Seq[Int] = registry.streamRange(0, 10)
+    val list: Seq[Int] = registry.listRange(0, 10)
+
+    // then
+    registry.streamAccessCounter should equal(1)
+    registry.listAccessCounter should equal(10)
+
+
+  }
+
+  test("stream creation recursive method should be 'n+1' times when resulting stream is accessed at index n") {
+
+    // given
+    val registry = new SeqAccessRegistry
+
+    // when
+    val stream: Stream[Int] = registry.streamRange(0, 10)
+    val list: Seq[Int] = registry.listRange(0, 10)
+    stream(3)
+    stream(1)
+    stream(2)
+    stream(2)
+    stream(2)
+
+    // then
+    registry.streamAccessCounter should equal(4)
+    registry.listAccessCounter should equal(10)
+
+  }
+
+}
---
+++ functional-programing-design-in-scala/week2/src/test/scala/coursera/fundesigninscala/week2/structuralinduction/IntSetTest.scala
@@ -0,0 +1,50 @@
+package coursera.fundesigninscala.week2.structuralinduction
+
+import org.scalatest.{FunSuite, Matchers}
+
+class IntSetTest extends FunSuite with Matchers {
+
+  test("set should be generated with internal structure as a binary tree") {
+
+    // given
+    val expectedTree = NonEmpty(4, NonEmpty(2, Empty, NonEmpty(3, Empty, Empty)), NonEmpty(5, Empty, Empty))
+
+    // when
+    val set: IntSet = Empty incl 4 incl 2 incl 3 incl 5
+
+    // then
+    set shouldBe a[NonEmpty]
+    set.asInstanceOf[NonEmpty] should equal(expectedTree)
+
+  }
+
+  test("set should not have repeated elements internally when repeated elements are included") {
+
+    // given
+    val expectedTree = NonEmpty(4, Empty, Empty)
+
+    // when
+    val set: IntSet = Empty incl 4 incl 4 incl 4
+
+    // then
+    set shouldBe a[NonEmpty]
+    set.asInstanceOf[NonEmpty] should equal(expectedTree)
+
+  }
+
+  test("contains function of set should work either on exiting elements or non existing elements") {
+
+    // given
+    val set: IntSet = Empty incl 4 incl 2 incl 3 incl 5
+
+    // when
+    val isFiveIncluded = set contains 5
+    val isTenIncluded = set contains 10
+
+    // then
+    isFiveIncluded should equal(true)
+    isTenIncluded should equal(false)
+
+  }
+
+}
